d445d6646afd5638caaa944a6508e0ecab71edb5
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-03-10 18:48:40 -0400

    i18n on cart page

--- src/app/[locale]/cart/page.tsx
+++ src/app/[locale]/cart/page.tsx
@@ -2,14 +2,17 @@ import CartProduct from "@/components/CartProduct";
 import Product from "@/components/Product";
 import Button from "@/components/interactive/Button";
 import { StarIcon } from "@heroicons/react/20/solid";
+import { useTranslations } from "next-intl";
 import { Fragment } from "react";
 
 export default function Cart() {
+    const t = useTranslations("Cart");
+
     return (
         <div className="grid grid-cols-2 gap-6 mt-6">
             <section className="col-span-2 xl:col-span-1">
                 <h1 className="text-xl font-semibold flex items-center justify-start">
-                    Shopping Cart
+                    {t("Shopping Cart")}
                 </h1>
                 <div className="bg-white rounded-lg p-2 drop-shadow-md mt-2 dark:bg-neutral-900/70">
                     {/* TODO: Make this show real products instead of 3 random ones */}
@@ -27,8 +30,8 @@ export default function Cart() {
                         </Fragment>
                     ))}
                     <span className="text-right w-full block mt-2">
-                        Subtotal (3 items):{" "}
-                        <span className="text-indigo-600 font-bold">
+                        {t("Subtotal")} (3 {t("Items")}):{" "}
+                        <span className="text-indigo-600 font-bold dark:text-indigo-400">
                             $56.97
                         </span>
                     </span>
@@ -36,17 +39,17 @@ export default function Cart() {
             </section>
             <section className="col-span-2 xl:col-span-1">
                 <h1 className="text-xl font-semibold flex items-center justify-start">
-                    Shopping Cart
+                    {t("Go to Checkout")}
                 </h1>
                 <div className="bg-white rounded-lg p-2 drop-shadow-md mt-2 dark:bg-neutral-900/70">
                     <span className="text-center text-lg w-full block">
-                        Subtotal (3 items):{" "}
-                        <span className="text-indigo-600 font-bold">
+                        {t("Subtotal")} (3 {t("Items")}):{" "}
+                        <span className="text-indigo-600 font-bold dark:text-indigo-400">
                             $56.97
                         </span>
                     </span>
                     <Button theme="primary" className="font-semibold mt-2">
-                        Go to Checkout
+                        {t("Go to Checkout")}
                     </Button>
                 </div>
             </section>
