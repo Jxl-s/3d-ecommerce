587de51c8f21ec33c8362ed7670732cdfc0c3b8f
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-03-10 18:44:57 -0400

    add cart page

--- src/app/[locale]/cart/page.tsx
+++ src/app/[locale]/cart/page.tsx
@@ -1,5 +1,6 @@
 import CartProduct from "@/components/CartProduct";
 import Product from "@/components/Product";
+import Button from "@/components/interactive/Button";
 import { StarIcon } from "@heroicons/react/20/solid";
 import { Fragment } from "react";
 
@@ -22,11 +23,31 @@ export default function Cart() {
                                 price={18.99}
                                 ratingCount={1423}
                             />
-                            {i < 2 && (
-                                <hr className="mx-2 dark:border-neutral-600/50" />
-                            )}
+                            <hr className="mx-2 dark:border-neutral-600/50" />
                         </Fragment>
                     ))}
+                    <span className="text-right w-full block mt-2">
+                        Subtotal (3 items):{" "}
+                        <span className="text-indigo-600 font-bold">
+                            $56.97
+                        </span>
+                    </span>
+                </div>
+            </section>
+            <section className="col-span-2 xl:col-span-1">
+                <h1 className="text-xl font-semibold flex items-center justify-start">
+                    Shopping Cart
+                </h1>
+                <div className="bg-white rounded-lg p-2 drop-shadow-md mt-2 dark:bg-neutral-900/70">
+                    <span className="text-center text-lg w-full block">
+                        Subtotal (3 items):{" "}
+                        <span className="text-indigo-600 font-bold">
+                            $56.97
+                        </span>
+                    </span>
+                    <Button theme="primary" className="font-semibold mt-2">
+                        Go to Checkout
+                    </Button>
                 </div>
             </section>
         </div>
